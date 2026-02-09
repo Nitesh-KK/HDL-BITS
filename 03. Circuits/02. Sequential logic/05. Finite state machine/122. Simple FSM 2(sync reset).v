cfea344e22fbef4e44efa27f5f4c09fa742aae62
Nitesh-KK <niteshkk.github@gmail.com>
2026-02-09 16:21:31 +0530

    Create 122. Simple FSM 2(sync reset).v

---
+++ 03. Circuits/02. Sequential logic/05. Finite state machine/122. Simple FSM 2(sync reset).v
@@ -0,0 +1,22 @@
+module top_module(
+    input clk,
+    input reset,  
+    input j,
+    input k,
+    output out); 
+
+    parameter OFF=1'b0, ON=1'b1; 
+    reg state, next_state;
+
+    always @(*) begin
+        case(state)
+            OFF:next_state=j?ON:OFF;
+            ON:next_state=k?OFF:ON;
+        endcase
+    end
+    always @(posedge clk) begin
+        if(reset) state<=OFF;
+        else state<=next_state;
+    end
+    assign out=(state==OFF)?1'b0:1'b1;
+endmodule
